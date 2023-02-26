892eabf490c25d04465fccc3f7aeca2cfb93c61e
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-02-26 23:42:23 +0800

    Updated Catalog

---
+++ README.md
@@ -0,0 +1,24 @@
+ISTD 50043 Group Project
+===========
+
+Welcome!
+
+## Labs
+Your project consists of four labs. You are *strongly advised* to start early.  
+
+* [Lab 1](lab1.md): due **Sunday, 5 March 11:59PM**.
+* [Lab 2](lab2.md): due **Sunday, 19 March 11:59PM**.
+* [Lab 3](lab3.md): due **9 April 11:59PM**. 
+* [Lab 4](lab4.md): due **23 April 11:59PM**. 
+
+Each lab is given **15 points**: **12** for passing all the tests, and **3** for the report. 
+
+## Important
+In addition to the academic integrity policy (as discussed in the lecture), take note that: 
+* You cannot make your Github repository for the labs public. 
+* You cannot copy and paste code from other people or any other sources. 
+* You cannot share your solutions.  
+
+
+### Acknowledgement
+The labs are modified from the MIT 6830 course. We thank the MIT staff for the materials.  
---
+++ src/java/simpledb/common/Catalog.java
@@ -0,0 +1,234 @@
+package simpledb.common;
+
+import simpledb.common.Type;
+import simpledb.storage.DbFile;
+import simpledb.storage.HeapFile;
+import simpledb.storage.TupleDesc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * The Catalog keeps track of all available tables in the database and their
+ * associated schemas.
+ * For now, this is a stub catalog that must be populated with tables by a
+ * user program before it can be used -- eventually, this should be converted
+ * to a catalog that reads a catalog table from disk.
+ * 
+ * @Threadsafe
+ */
+public class Catalog {
+
+    /**
+     * Constructor.
+     * Creates a new, empty catalog.
+     */
+
+
+    private class Table {
+        private DbFile file;
+        private String name;
+        private String pkeyField;
+
+        public Table(DbFile file, String name, String pkeyField) {
+            this.file = file;
+            this.name = name;
+            this.pkeyField = pkeyField;
+        }
+
+        public DbFile getFile() {
+            return this.file;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getPkeString() {
+            return this.pkeyField;
+        }
+    }
+
+    // private ArrayList<Table> tableList;
+    private HashMap<String, Integer> tableNameMap;
+    private HashMap<Integer, Table> tableIdMap;
+
+    public Catalog() {
+        // some code goes here
+        // tableList = new ArrayList<Table>();
+        tableNameMap = new HashMap<String, Integer>();
+        tableIdMap = new HashMap<Integer, Table>();
+    }
+
+    /**
+     * Add a new table to the catalog.
+     * This table's contents are stored in the specified DbFile.
+     * If there exists a table with the same name or ID, replace that old table with this one. 
+     * @param file the contents of the table to add;  file.getId() is the identfier of
+     *    this file/tupledesc param for the calls getTupleDesc and getFile. 
+     * @param name the name of the table -- may be an empty string.  May not be null.  
+     * @param pkeyField the name of the primary key field
+     */
+    public void addTable(DbFile file, String name, String pkeyField) {
+        // some code goes here
+        Table table = new Table(file, name, pkeyField);
+        // for (Table i : tableList) {
+        //     if(i.getFile().getId() == file.getId() || i.getName() == name){
+        //         this.tableList.remove(i);
+        //     }
+        // }
+        // this.tableList.add(table);
+        if(tableNameMap.containsKey(name)){
+            tableNameMap.replace(name, file.getId());
+        }
+        else{
+            tableNameMap.put(name, file.getId());
+
+        }
+        if (tableIdMap.containsKey(file.getId())){
+            tableIdMap.replace(file.getId(), table);
+        }
+        else{
+            tableIdMap.put(file.getId(), table);
+        }
+    }
+
+    public void addTable(DbFile file, String name) {
+        addTable(file, name, "");
+    }
+
+    /**
+     * Add a new table to the catalog.
+     * This table has tuples formatted using the specified TupleDesc and its
+     * contents are stored in the specified DbFile.
+     * @param file the contents of the table to add;  file.getId() is the identfier of
+     *    this file/tupledesc param for the calls getTupleDesc and getFile
+     */
+    public void addTable(DbFile file) {
+        addTable(file, (UUID.randomUUID()).toString());
+    }
+
+    /**
+     * Return the id of the table with a specified name,
+     * @throws NoSuchElementException if the table doesn't exist
+     */
+    public int getTableId(String name) throws NoSuchElementException {
+        // some code goes here
+        // int TableId = -1;
+        // for (Table i: tableList){
+        //     if(i.getName() == name){
+        //         TableId = i.getFile().getId();
+        //     }
+        // }
+        // return TableId;
+        return tableNameMap.get(name);
+    }
+
+    /**
+     * Returns the tuple descriptor (schema) of the specified table
+     * @param tableid The id of the table, as specified by the DbFile.getId()
+     *     function passed to addTable
+     * @throws NoSuchElementException if the table doesn't exist
+     */
+    public TupleDesc getTupleDesc(int tableid) throws NoSuchElementException {
+        // some code goes here 
+
+        return tableIdMap.get(tableid).getFile().getTupleDesc();
+    }
+
+    /**
+     * Returns the DbFile that can be used to read the contents of the
+     * specified table.
+     * @param tableid The id of the table, as specified by the DbFile.getId()
+     *     function passed to addTable
+     */
+    public DbFile getDatabaseFile(int tableid) throws NoSuchElementException {
+        // some code goes here
+        
+        return tableIdMap.get(tableid).getFile();
+    }
+
+    public String getPrimaryKey(int tableid) {
+        // some code goes here
+
+        return tableIdMap.get(tableid).getPkeString();
+    }
+
+    public Iterator<Integer> tableIdIterator() {
+        // some code goes here
+        return this.tableIdMap.keySet().iterator();
+    }
+
+    public String getTableName(int id) {
+        // some code goes here
+        return tableIdMap.get(id).getName();
+    }
+    
+    /** Delete all tables from the catalog */
+    public void clear() {
+        // some code goes here
+        tableNameMap.clear();
+        tableIdMap.clear();
+    }
+    
+    /**
+     * Reads the schema from a file and creates the appropriate tables in the database.
+     * @param catalogFile
+     */
+    public void loadSchema(String catalogFile) {
+        String line = "";
+        String baseFolder=new File(new File(catalogFile).getAbsolutePath()).getParent();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(catalogFile));
+            
+            while ((line = br.readLine()) != null) {
+                //assume line is of the format name (field type, field type, ...)
+                String name = line.substring(0, line.indexOf("(")).trim();
+                //System.out.println("TABLE NAME: " + name);
+                String fields = line.substring(line.indexOf("(") + 1, line.indexOf(")")).trim();
+                String[] els = fields.split(",");
+                ArrayList<String> names = new ArrayList<>();
+                ArrayList<Type> types = new ArrayList<>();
+                String primaryKey = "";
+                for (String e : els) {
+                    String[] els2 = e.trim().split(" ");
+                    names.add(els2[0].trim());
+                    if (els2[1].trim().equalsIgnoreCase("int"))
+                        types.add(Type.INT_TYPE);
+                    else if (els2[1].trim().equalsIgnoreCase("string"))
+                        types.add(Type.STRING_TYPE);
+                    else {
+                        System.out.println("Unknown type " + els2[1]);
+                        System.exit(0);
+                    }
+                    if (els2.length == 3) {
+                        if (els2[2].trim().equals("pk"))
+                            primaryKey = els2[0].trim();
+                        else {
+                            System.out.println("Unknown annotation " + els2[2]);
+                            System.exit(0);
+                        }
+                    }
+                }
+                Type[] typeAr = types.toArray(new Type[0]);
+                String[] namesAr = names.toArray(new String[0]);
+                TupleDesc t = new TupleDesc(typeAr, namesAr);
+                HeapFile tabHf = new HeapFile(new File(baseFolder+"/"+name + ".dat"), t);
+                addTable(tabHf,name,primaryKey);
+                System.out.println("Added table : " + name + " with schema " + t);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println ("Invalid catalog entry : " + line);
+            System.exit(0);
+        }
+    }
+}
+
