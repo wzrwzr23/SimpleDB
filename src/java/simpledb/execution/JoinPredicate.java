a0c9f938a4a036419b59e622f884486e6851cd02
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-03-17 22:09:01 +0800

    feat: Task 1 completed

--- src/java/simpledb/execution/JoinPredicate.java
+++ src/java/simpledb/execution/JoinPredicate.java
@@ -27,8 +27,14 @@ public class JoinPredicate implements Serializable {
      *            Predicate.Op.LESS_THAN_OR_EQ
      * @see Predicate
      */
+    private int field1;
+    private Predicate.Op op;
+    private int field2;
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        this.field1 = field1;
+        this.field2 = field2;
+        this.op = op;
     }
 
     /**
@@ -39,24 +45,26 @@ public class JoinPredicate implements Serializable {
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        Field f1 = t1.getField(this.field1);
+        Field f2 = t2.getField(this.field2);
+        return f1.compare(this.op, f2);
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return this.field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return this.field2;
     }
     
     public Predicate.Op getOperator()
     {
         // some code goes here
-        return null;
+        return this.op;
     }
 }
